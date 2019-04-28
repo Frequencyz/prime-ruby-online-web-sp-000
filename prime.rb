64a3281011d6380779fe1efde206c4188d97a9c4
Andrew Bay <daddnbay@yahoo.com>
2019-04-28 22:34:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,14 @@
-
+def prime?(num)
+  if num < 1 || num == 1
+    return false
+  elsif num > 1
+    for i in 2...(num)
+      if (num % i) == 0
+        return false
+      end
+    end
+  end
+    true
+end
    
   
